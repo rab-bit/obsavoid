db7450071ad8cd9330bd86785c63ab1832fae503
Rondinele Prado <rondinele_prado@gmx.com>
2016-11-23 18:03:47 -0200

    correcoes

--- src/obstacle_avoidance.cpp
+++ src/obstacle_avoidance.cpp
@@ -23,7 +23,7 @@ sensor_msgs::LaserScan scan_mem;
 **/
 void obstacleAvoidanceControl(geometry_msgs::Twist* twist_teleop){
     
-    int numero_amostras = (int) floor((scan_mem.angle_max - scan.angle_min) / scan_mem.angle_increment);
+    int numero_amostras = (int) floor((scan_mem.angle_max - scan_mem.angle_min) / scan_mem.angle_increment);
     
     int amostra = (int)floor(numero_amostras/2); // Leitura de 0 rad
     float dist_obstaculo = scan_mem.ranges[amostra];
